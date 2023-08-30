d41040c37dd2c15bbba0d3ef8e8b3ff455cf4b5d
Stelsor95 <silaa7465@gmail.com>
2023-08-30 14:53:39 +0300

    Взрыв мозга

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿void ArrayToThreeChar(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < array[i].Length; j++)
+            count++;
+        if (count < 4)
+            Console.Write($"{array[i]}, ");
+    }
+}
+string[] arr = { "hello", "2", "world", ":-)" };
+ArrayToThreeChar(arr);
